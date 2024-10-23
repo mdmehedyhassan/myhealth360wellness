84863d1fb8b630feb9b9e158e7cf9a3ded40243d
Molla Sayem <developersayem012@gmail.com>
2024-10-24 02:17:13 +0600

    test

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <div className="text-center bg-blue-500 text-white p-4">
         <h1 className="text-3xl font-bold">Hello, Tailwind CSS!</h1>
         <button className="bg-slate-200">Hello world</button>
+        <button className="bg-slate-200">New Button</button>
       </div>
     </div>
   );
