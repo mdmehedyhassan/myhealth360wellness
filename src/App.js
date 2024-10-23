fd9b50a3c9b847254a092a478de6447af75e3da1
mdmehedyhassan <mdmehedyh55@gmail.com>
2024-10-24 01:48:29 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="text-center bg-blue-500 text-white p-4">
+        <h1 className="text-3xl font-bold">Hello, Tailwind CSS!</h1>
+        <button>Hello world</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
