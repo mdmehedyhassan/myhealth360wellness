1fa60791f86c2c790fd85ab1cc35e1d5e993e80a
Molla Sayem <developersayem012@gmail.com>
2024-10-24 15:14:48 +0600

    lorem added in contact header

--- src/page/contact/ContactHeader/ContactHeader.js
+++ src/page/contact/ContactHeader/ContactHeader.js
@@ -4,6 +4,7 @@ function ContactHeader() {
   return (
     <div>
       <h1>Hello ContactHeader</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore ipsum, nesciunt exercitationem doloremque quaerat quod magni saepe facere nam molestias beatae. Laborum accusantium perspiciatis atque quis deleniti ipsam? Vero, eos. </p>
     </div>
   )
 }
