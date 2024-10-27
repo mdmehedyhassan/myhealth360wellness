a054b9f9c10a7fc013c76d1de9d21646d943e435
Mr-RoyShuvro <shuvroroyshuvo@gmail.com>
2024-10-27 15:40:24 +0600

    added banner and updated navbar

--- src/component/share/Header/Header.js
+++ src/component/share/Header/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import Navbar from './Navbar';
+import Banner from './Banner';
 
 function Header() {
   return (
     <div>
       <Navbar></Navbar>
-      <ul className='flex gap-10 mb-10'>
+      <Banner></Banner>
+      {/* <ul className='flex gap-10 mb-10'>
         <li><Link to="/"> Home </Link></li>
         <li><Link to="/about"> About </Link></li>
         <li><Link to="/service"> Our Services </Link></li>
@@ -14,7 +16,7 @@ function Header() {
         <li><Link to="/contact"> Contact </Link></li>
         <li><Link to="/services/iv-infusion-bar"> IV Infusion Bar Page </Link></li>
       </ul>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore ipsum, nesciunt exercitationem doloremque quaerat quod magni saepe facere nam molestias beatae. Laborum accusantium perspiciatis atque quis deleniti ipsam? Vero, eos. </p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore ipsum, nesciunt exercitationem doloremque quaerat quod magni saepe facere nam molestias beatae. Laborum accusantium perspiciatis atque quis deleniti ipsam? Vero, eos. </p> */}
     </div>
   )
 }
--- src/component/share/Header/Navbar.jsx
+++ src/component/share/Header/Navbar.jsx
@@ -3,71 +3,73 @@ import logo from '../../../img/logo.png'
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor">
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M4 6h16M4 12h8m-8 6h16" />
-                        </svg>
+        <div>
+            <div className="navbar absolute border-b-2 px-2 md:px-16">
+                <div className="navbar-start">
+                    <div className="dropdown ">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow ">
+                            <li className='text-white font-normal text-base'><a>Home</a></li>
+                            <li className='text-white font-normal text-base'><a>About</a></li>
+                            <li className='text-white font-normal text-base'>
+                                <a>Our Services</a>
+                                <ul className="p-2">
+                                    <li className='text-white font-normal text-base'><a>Service 1</a></li>
+                                    <li className='text-white font-normal text-base'><a>Service 2</a></li>
+                                    <li className='text-white font-normal text-base'><a>Service 3</a></li>
+                                    <li className='text-white font-normal text-base'><a>Service 4</a></li>
+                                    <li className='text-white font-normal text-base'><a>Service 5</a></li>
+                                </ul>
+                            </li>
+                            <li className='text-white font-normal text-base'><a>Memberships</a></li>
+                            <li className='text-white font-normal text-base'><a>Packages</a></li>
+                            <li className='text-white font-normal text-base'><a>Contact</a></li>
+                        </ul>
                     </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <li><a>Home</a></li>
-                        <li><a>About</a></li>
-                        <li>
-                            <a>Our Services</a>
-                            <ul className="p-2">
-                                <li><a>Service 1</a></li>
-                                <li><a>Service 2</a></li>
-                                <li><a>Service 3</a></li>
-                                <li><a>Service 4</a></li>
-                                <li><a>Service 5</a></li>
-                            </ul>
+                    <a className="btn btn-ghost mb-3 mt-5 w-52 h-20">
+                        <img className='w-48 h-auto' src={logo} alt="" />
+                    </a>
+                </div>
+                <div className="navbar-end hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 z-[1]">
+                        <li className='text-white font-normal text-base'><a>Home</a></li>
+                        <li className='text-white font-normal text-base'><a>About</a></li>
+                        <li className='text-white font-normal text-base'>
+                            <details>
+                                <summary>Our Services</summary>
+                                <ul className="p-2">
+                                    <li className='text-white font-normal text-base'><a>Service 1</a></li>
+                                    <li className='text-white font-normal text-base'><a>Service 2</a></li>
+                                    <li className='text-white font-normal text-base'><a>Service 3</a></li>
+                                    <li className='text-white font-normal text-base'><a>Service 4</a></li>
+                                    <li className='text-white font-normal text-base'><a>Service 5</a></li>
+                                </ul>
+                            </details>
                         </li>
-                        <li><a>Memberships</a></li>
-                        <li><a>Packages</a></li>
-                        <li><a>Contact</a></li>
+                        <li className='text-white font-normal text-base'><a>Memberships</a></li>
+                        <li className='text-white font-normal text-base'><a>Packages</a></li>
+                        <li className='text-white font-normal text-base'><a>Contact</a></li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">
-                    <img className='w-32 h-auto' src={logo} alt="" />
-                    </a>
-            </div>
-            <div className="navbar-end hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Home</a></li>
-                    <li><a>About</a></li>
-                    <li>
-                        <details>
-                            <summary>Our Services</summary>
-                            <ul className="p-2">
-                                <li><a>Service 1</a></li>
-                                <li><a>Service 2</a></li>
-                                <li><a>Service 3</a></li>
-                                <li><a>Service 4</a></li>
-                                <li><a>Service 5</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Memberships</a></li>
-                    <li><a>Packages</a></li>
-                    <li><a>Contact</a></li>
-                </ul>
-            </div>
-            {/* <div className="navbar-end">
+                {/* <div className="navbar-end">
                 <a className="btn">Button</a>
             </div> */}
+            </div>
         </div>
     );
 };
