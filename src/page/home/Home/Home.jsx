6337bffb97d47bc07ac9d597353c6aec26c42951
Molla Sayem <developersayem012@gmail.com>
2024-10-30 19:08:42 +0600

    [Home]Membership-Component

--- src/page/home/Home/Home.jsx
+++ src/page/home/Home/Home.jsx
@@ -3,6 +3,7 @@ import FeaturedServicesCom from '../FeaturedCom/FeaturedServicesCom';
 import TreatmentCom from '../TreatmentCom/TreatmentCom';
 import Banner from '../../../component/share/Header/Banner';
 import AboutDoctorCom from '../AboutDoctorCom/AboutDoctorCom';
+import MembershipCom from '../MembershipCom/MembershipCom';
 
 function Home() {
   
@@ -13,6 +14,7 @@ function Home() {
     <FeaturedServicesCom/>
     <TreatmentCom/>
     <AboutDoctorCom/>
+    <MembershipCom/>
     </div>
   )
 }
