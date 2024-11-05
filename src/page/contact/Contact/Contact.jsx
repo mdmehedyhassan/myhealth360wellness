2c1a5111d3b1231893ad9437ec670f50a1100c91
Mr-RoyShuvro <shuvroroyshuvo@gmail.com>
2024-11-05 22:02:33 +0600

    Added contact information with contact form

--- src/page/contact/Contact/Contact.jsx
+++ src/page/contact/Contact/Contact.jsx
@@ -1,23 +1,50 @@
 import React from 'react';
 import ContactBanner from './ContactBanner';
+import logo1 from '../../../img/Contact/logo1.png';
+import logo2 from '../../../img/Contact/logo2.png';
+import logo3 from '../../../img/Contact/logo3.png';
+import logo4 from '../../../img/Contact/logo4.png';
 
 const Contact = () => {
   return (
     <div>
       <ContactBanner></ContactBanner>
-      <div className="hero bg-[#E9F0EC] min-h-screen">
-        <div className="hero-content flex-col lg:flex-row ">
+      <div className="hero bg-[#E9F0EC] pt-[135px] pb-[120px] ">
+        <div className="hero-content flex-col lg:flex-row gap-[89px]">
           {/* Contact Information */}
-          <div className="text-center lg:text-left w-1/2">
-            <h1 className="text-5xl font-bold">Contact</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-              quasi. In deleniti eaque aut repudiandae et a id nisi.
-            </p>
+          <div className='flex flex-col gap-[30px]'>
+            <div className='flex items-center gap-[26px] w-[453px] p-[17px] py-5 border-[#B6CCE2] border-[1px] rounded-[10px]'>
+              <img className='w-[84px] h-[84px]' src={logo1} alt="" />
+              <div>
+                <h3 className='text-2xl font-semibold leading-[120%] text-[#060F13] mb-[7px]'>Phone Number</h3>
+                <p className='text-[15px] font-normal leading-[150%] text-[#5E6669]'>+012 3456 7890</p>
+              </div>
+            </div>
+            <div className='flex items-center gap-[26px] w-[453px] p-[17px] py-5 border-[#B6CCE2] border-[1px] rounded-[10px]'>
+              <img className='w-[84px] h-[84px]' src={logo2} alt="" />
+              <div>
+                <h3 className='text-2xl font-semibold leading-[120%] text-[#060F13] mb-[7px]'>Email Address</h3>
+                <p className='text-[15px] font-normal leading-[150%] text-[#5E6669]'>emailaddress@email.com</p>
+              </div>
+            </div>
+            <div className='flex items-center gap-[26px] w-[453px] p-[17px] py-5 border-[#B6CCE2] border-[1px] rounded-[10px]'>
+              <img className='w-[84px] h-[84px]' src={logo3} alt="" />
+              <div>
+                <h3 className='text-2xl font-semibold leading-[120%] text-[#060F13] mb-[7px]'>Working Time</h3>
+                <p className='text-[15px] font-normal leading-[150%] text-[#5E6669]'>10AM  to 8PM</p>
+              </div>
+            </div>
+            <div className='flex items-center gap-[26px] w-[453px] p-[17px] py-5 border-[#B6CCE2] border-[1px] rounded-[10px]'>
+              <img className='w-[84px] h-[84px]' src={logo4} alt="" />
+              <div>
+                <h3 className='text-2xl font-semibold leading-[120%] text-[#060F13] mb-[7px]'>Location</h3>
+                <p className='text-[15px] font-normal leading-[150%] text-[#5E6669]'>2464 Royal Ln. Mesa, New Jersey 45463</p>
+              </div>
+            </div>
           </div>
           {/* Form */}
-          <div className="card bg-white shrink-0 shadow-2xl w-1/2">
-            <form className="card-body">
+          <div className="bg-white border-[1px] border-[#B6CCE2] rounded-[20px] min-h-[586px]">
+            <form className="pt-[35px] pb-[40px] pl-[42px] pr-[41px]">
 
               <div className="form-control">
                 <label className="label">
