915eb682769ffd2397e3a2db181cc51e6785fc08
Molla Sayem <developersayem012@gmail.com>
2024-11-20 22:28:21 +0600

    [IV Infusion Bar]Color name changed

--- src/services-page/iv-infusion-bar/TherapyBookCom/TherapyBookCom.jsx
+++ src/services-page/iv-infusion-bar/TherapyBookCom/TherapyBookCom.jsx
@@ -33,20 +33,3 @@ const TherapyBookCom = () => {
 };
 
 export default TherapyBookCom;
-
-// className=" flex flex-col justify-center items-center bg-no-repeat bg-cover bg-center"
-// style={{
-//   backgroundImage: `url("https://i.ibb.co.com/hLstWXt/theraaapy-book-img.png")`,
-// }}
-
-{
-  /* <h1 className="text-center font-bold text-2xl lg:text-5xl px-5">
-          READY FOR IV THERAPY?
-        </h1>
-        <p className="text-sm my-5 mx-8 text-center">
-          Call now to Schedule a Consult with our qualified physician.
-        </p>
-        <button className="text-black text-sm lg:text-lg font-bold py-4 px-16 bg-white border rounded-full my-3">
-          Book an Infusion Suite!
-        </button> */
-}
