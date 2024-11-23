0cf499036bedcc5389cf860eaa030a4faaac0e97
Molla Sayem <developersayem012@gmail.com>
2024-11-23 19:45:25 +0600

    [about]color changed

--- src/page/about/VisionMissionCom/VisionMissionCom.jsx
+++ src/page/about/VisionMissionCom/VisionMissionCom.jsx
@@ -4,18 +4,18 @@ import MissionImg from "../../../img/About/mission.png";
 
 function Card({ title, description, image, url }) {
   return (
-    <div className="w-[385px] lg:w-[643px] h-[172px] lg:h-[340px] rounded-xl bg-[#D9E5DE] border border-[#87afac] ">
+    <div className="w-[385px] lg:w-[643px] h-[172px] lg:h-[340px] rounded-xl bg-bgSecondary border border-borderPrimary ">
       <div className="w-full flex justify-between items-center p-2 lg:p-5">
         {/* Text Section */}
         <div className="">
-          <h2 className="text-[20px] lg:text-[40px] font-bold text-[#0B5E62] mb-2 lg:mb-4">
+          <h2 className="text-[20px] lg:text-[40px] font-bold text-primary mb-2 lg:mb-4">
             {title}
           </h2>
-          <p className="text-[12px] lg:text-[15px] text-[#333] mb-3 lg:mb-6 pr-2">
+          <p className="text-[12px] lg:text-[15px] text-textSecondary mb-3 lg:mb-6 pr-2">
             {description}
           </p>
           <a href={url}>
-            <button className="px-5 py-2 border border-[#0B5E62] text-[#000] rounded-full hover:bg-[#0B5E62] hover:text-white transition-colors duration-300 text-sm lg:text-lg">
+            <button className="px-5 py-2 border border-primary text-black rounded-full hover:bg-primary hover:text-white transition-colors duration-300 text-sm lg:text-lg">
               Learn More
             </button>
           </a>
--- src/page/about/WhyWeAreCom/WhyWeAreCom.jsx
+++ src/page/about/WhyWeAreCom/WhyWeAreCom.jsx
@@ -1,21 +1,47 @@
 import React from "react";
-import WhoWeAreImg from "../../../img/About/whoweare.png";
+import WhoWeAreImg from "../../../img/About/who-we-are-img.png";
+import PeoplesLogo from "../../../img/About/peoples-logo.png";
+import GroupLogo from "../../../img/About/group-logo.png";
 
 const WhyWeAreCom = () => {
   return (
     <div className="py-10 pb-16 lg:pb-32 mx-5 flex justify-center items-center ">
       <div className="grid grid-cols-1 lg:grid-cols-2 items-center lg:max-w-[1324px] gap-10">
         {/* Image Section */}
-        <div className="w-full hidden lg:block p-16">
+        <div className="hidden relative lg:flex justify-center items-center">
+          <div className="absolute top-10 right-10 text-white p-2 rounded-md w-[274px] h-[90px]">
+            <div className="w-full h-full bg-primary rounded-lg flex justify-start items-center p-3 gap-2 ">
+              <img className="rounded-xl" src={PeoplesLogo} alt="about-img" />
+              <h1 className="text-md font-bold text-wrap">8k+ Reviews</h1>
+            </div>
+          </div>
+          <img className="rounded-xl" src={WhoWeAreImg} alt="about-img" />
+
+          <div className="absolute bottom-0 left-11 text-white rounded-md w-[274px] h-[90px] p-2">
+            <div className="w-full h-full bg-primary rounded-lg grid grid-cols-5 items-center p-2 gap-2 ">
+              <div className="w-fit h-fit bg-bgPrimary p-3 rounded-lg col-span-1">
+                <img className="rounded-xl" src={GroupLogo} alt="about-img" />
+              </div>
+              <div className="col-span-4">
+                <h1 className="text-md font-bold">30,000+</h1>
+                <p className="text-[10px] font-bold ">
+                  By refining your ideal client profile and using precise
+                  filters
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <div className="w-full hidden lg:block p-16">
           <img
             src={WhoWeAreImg}
             alt="HealthImg"
             className="rounded-lg object-cover w-full h-full"
           />
-        </div>
+        </div> */}
         {/* Text Section */}
         <div className="w-full">
-          <h2 className="text-3xl font-bold text-[#0B5E62] mb-4 ">Who we are</h2>
+          <h2 className="text-3xl font-bold text-primary mb-4 ">Who we are</h2>
           {/* Image Section */}
           <div className="w-full lg:hidden">
             <img
@@ -24,7 +50,7 @@ const WhyWeAreCom = () => {
               className="rounded-lg object-cover w-full h-full"
             />
           </div>
-          <p className="text-[#333] mb-3 leading-relaxed mt-[30px] md:mt-0">
+          <p className="text-textSecondary mb-3 leading-relaxed mt-[30px] md:mt-0">
             We are are Medical Spa that offers wellness care customized to your
             personal goals as part of our larger vision for your health and
             happiness to enable your eu zen, “living your best.” We are nestled
@@ -32,7 +58,7 @@ const WhyWeAreCom = () => {
             highly personalized and above average experience for all your
             wellness and aesthetic needs!
           </p>
-          <p className="text-[#333] mb-6 leading-relaxed">
+          <p className="text-textSecondary mb-6 leading-relaxed">
             We are are Medical Spa that offers wellness care customized to your
             personal goals as part of our larger vision for your health and
             happiness to enable your eu zen, “living your best.” We are nestled
@@ -45,7 +71,7 @@ const WhyWeAreCom = () => {
             Clarence, NY and will facilitate a highly personalized and above
             average experience for all your wellness and aesthetic needs!
           </p>
-          <button className="text-[#0B5E62] text-sm lg:text-lg font-bold py-4 px-16 bg-transparent border border-[#0B5E62] rounded-full hover:bg-[#0B5E62] hover:text-white">
+          <button className="text-pretty text-sm lg:text-lg font-bold py-4 px-16 bg-transparent border border-primary rounded-full hover:bg-primary hover:text-white">
             Learn More
           </button>
         </div>
