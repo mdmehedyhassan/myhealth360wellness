6dd484649c75a5279fad96db58382d0af3d57727
Molla Sayem <developersayem012@gmail.com>
2024-11-26 23:30:26 +0600

    [fixed]WhyShouldCom

--- src/services-page/iv-infusion-bar/WhyShouldCom/WhyShouldCom.jsx
+++ src/services-page/iv-infusion-bar/WhyShouldCom/WhyShouldCom.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import WhyShouldImg from "../../../img/IV-Infusion-Bar/whyshould.png";
+import WhyShouldImg1 from "../../../img/IV-Infusion-Bar/why-should-img-1.png";
+import WhyShouldImg2 from "../../../img/IV-Infusion-Bar/why-should-img-2.png";
+import DotCom from "../../../component/share/DotCom/DotCom";
 
 const WhyShouldCom = () => {
   return (
@@ -42,8 +44,43 @@ const WhyShouldCom = () => {
             <span>Schedule a Consult</span>
           </button>
         </div>
-        <div className="flex justify-center items-center p-5 lg:p-16 ">
-          <img className="rounded-xl" src={WhyShouldImg} alt="about-img" />
+
+        {/* Image Section For Desktop*/}
+        <div className="relative mx-auto">
+          <div className="w-[248.37px] lg:w-[415px] h-[268.72px] lg:h-[449px] p-5 bg-bgPrimary rounded-lg border border-borderPrimary relative">
+            {/* Dot Grid for desktop */}
+            <div className="hidden lg:block w-[170px] h-[81px]  bg-bgPrimary absolute -bottom-[35px] -left-[2rem] p-2 z-0">
+              <div className="w-full h-full grid grid-cols-10 gap-0 justify-items-center items-center">
+                {Array.from({ length: 40 }).map((_, index) => (
+                  <DotCom key={index} />
+                ))}
+              </div>
+            </div>
+            {/* Dot Grid for Mobile */}
+            <div className="lg:hidden w-[106.36px] h-[52.49px]  bg-bgPrimary absolute -bottom-[21px] -left-[1.5rem]  p-2 z-0">
+              <div className="w-full h-full grid grid-cols-6 gap-0 justify-items-center items-center">
+                {Array.from({ length: 24 }).map((_, index) => (
+                  <DotCom key={index} />
+                ))}
+              </div>
+            </div>
+            <div className="flex justify-evenly relative">
+              {/* Left Section */}
+              <div className="absolute -top-10 -left-[4rem] lg:-left-24">
+                {/* Left Image */}
+                <div className="w-[155.61px] lg:w-[260px] h-[243.53px] lg:h-[406.91px] ">
+                  <img src={WhyShouldImg1} alt="about-img" />
+                </div>
+              </div>
+              {/* Right Section */}
+              <div className="z-10 absolute top-0 -right-[3.5rem] lg:-right-20">
+                {/* Right Image */}
+                <div className="w-[153.84px] lg:w-[257.04px] h-[229.22px] lg:h-[383px]">
+                  <img src={WhyShouldImg2} alt="about-img" />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
